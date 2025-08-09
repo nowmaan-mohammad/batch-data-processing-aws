d46f59060eaea578cd01816a4bac1e72dd06c30d
Nowmaan <nowmaan123@gmail.com>
2025-08-10 02:14:08 +0530

    created glue job to read/write s3

---
+++ python_scripts/GlueReadFromS3_job.py
@@ -0,0 +1,99 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsgluedq.transforms import EvaluateDataQuality
+from pyspark.sql.functions import lit
+from awsglue.dynamicframe import DynamicFrame
+import logging
+
+logger = logging.getLogger('my_logger')
+logger.setLevel(logging.INFO)
+
+# Create a handler for CloudWatch
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+logger.info('My log message')
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Default ruleset used by all target nodes with data quality enabled
+DEFAULT_DATA_QUALITY_RULESET = """
+    Rules = [
+        ColumnCount > 0
+    ]
+"""
+
+# Script generated for node data catalog
+datacatalog_node1754770179465 = glueContext.create_dynamic_frame.from_catalog(database="mydatabase", table_name="product", transformation_ctx="datacatalog_node1754770179465")
+
+logger.info('print schema of datacatalog_node1754770179465')
+datacatalog_node1754770179465.printSchema()
+
+count = datacatalog_node1754770179465.count()
+logger.info('count for frame is {}'.format(count))
+
+
+
+# Script generated for node Change Schema
+ChangeSchema_node1754770454150 = ApplyMapping.apply(frame=datacatalog_node1754770179465, mappings=[("marketplace", "string", "new_marketplace", "string"), ("customer_id", "long", "new_customer_id", "long"), ("product_id", "string", "new_product_id", "string"), ("seller_id", "string", "new_seller_id", "string"), ("sell_date", "string", "new_sell_date", "string"), ("quantity", "long", "new_quantity", "long"), ("year", "string", "new_year", "string")], transformation_ctx="ChangeSchema_node1754770454150")
+
+
+ResolveChoice_node = ChangeSchema_node1754770454150.resolveChoice(specs = [('new_seller_id','cast:long')],
+transformation_ctx="ResolveChoice_node"
+)
+
+logger.info('print schema of ResolveChoice_node')
+ResolveChoice_node.printSchema()
+
+
+#convert dynamic dataframe into spark dataframe
+logger.info('convert dynamic dataframe ResolveChoice_node into spark dataframe')
+
+spark_data_frame=ResolveChoice_node.toDF()
+
+#apply spark where clause
+logger.info('filter rows with  where new_seller_id is not null')
+spark_data_frame_filter = spark_data_frame.where("new_seller_id is NOT NULL")
+
+
+# Add the new column to the data frame
+logger.info('create new column status with Active value')
+spark_data_frame_filter = spark_data_frame_filter.withColumn("new_status", lit("Active"))
+
+spark_data_frame_filter.show()
+
+
+logger.info('convert spark dataframe into table view product_view. so that we can run sql ')
+spark_data_frame_filter.createOrReplaceTempView("product_view")
+
+logger.info('create dataframe by spark sql ')
+
+product_sql_df = spark.sql("SELECT new_year,count(new_customer_id) as cnt,sum(new_quantity) as qty FROM product_view group by new_year ")
+
+logger.info('display records after aggregate result')
+product_sql_df.show()
+
+# Convert the data frame back to a dynamic frame
+logger.info('convert spark dataframe to dynamic frame ')
+dynamic_frame = DynamicFrame.fromDF(product_sql_df, glueContext, "dynamic_frame")
+
+
+
+logger.info('dynamic frame uploaded in bucket myglue-etl-project/output/newproduct/ in parquet format ')
+
+# Script generated for node S3 bucket
+EvaluateDataQuality().process_rows(frame=dynamic_frame, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1754770153397", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+
+S3bucket_node1754770183324 = glueContext.write_dynamic_frame.from_options(frame=dynamic_frame, connection_type="s3", format="glueparquet", connection_options={"path": "s3://batch-data-glue/output/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="S3bucket_node1754770183324")
+
+job.commit()
